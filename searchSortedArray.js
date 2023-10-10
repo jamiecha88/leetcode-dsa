14d0426a2f0ee03f66cd2cf25b1fe77f74e5ec3a
Jamie Cha <chaeunhee98@gmail.com>
2023-10-09 23:55:14 -0500

    revise search sorted array problem

--- searchSortedArray.js
+++ searchSortedArray.js
@@ -5,23 +5,19 @@ EXAMPLES
 search([1,2,3,4,5,6],4) // 3
 search([1,2,3,4,5,6],6) // 5
 search([1,2,3,4,5,6],11) // -1
-
 */
 
 function searchArr(arr, num) {
-  if (Array.isArray(arr) && typeof num === "number") {
-    if (arr.length === 0) {
-      //checking if inputs are valid
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === num) {
-          return arr[i]; //incorrect; return index where value is located with `return i` instead
+  if (Array.isArray(arr) && typeof num === 'number') {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === num) {
+                return i; // return index where the num is located
+            }
         }
-      }
+        return -1; // if the value is not found in the array
+    } else {
+        return -1; // for invalid inputs
     }
-    return -1; //if value not found in array
-  } else {
-    return -1; //for anything else
-  }
 }
 
 console.log(searchArr([1, 2, 3, 4, 5, 6], 6)); // Expected output: 5
